430552ca1266569e81e5da74eecc15c55226c8e1
DAMULIHUE <lihue@outlook.com.br>
2025-04-02 17:53:17 +0100

    add somecoisas

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculo(){
+    let input = document.getElementById("input").value;
+    let result = "";
+
+    let inputArray = input.split("");
+
+    inputArray.forEach((val, index) => {
+        
+    })
+}
